9088022c5c1cfeaf9da5e134bac5283944a61271
Dean Stigall <stigallrealestate@gmail.com>
2017-11-17 02:04:47 +0000

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -22,8 +22,6 @@ describe('index.js', function () {
       expect(spy.calledWithExactly('bob')).to.be.true;
     });
 
-    it('invokes the console.log with the `driver` variable as the first argument and `is the driver variable name` as the second argument', function () {
-      expect(spy.calledWithExactly('bob', 'is the driver variable name')).to.be.true;
-    });
+
   });
 });
